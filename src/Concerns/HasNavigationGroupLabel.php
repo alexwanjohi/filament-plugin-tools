cdd893a7ca790597f92a486fe4e254fd2f8ea6dd
alexwanjohi <97999578+alexwanjohi@users.noreply.github.com>
2026-04-29 09:12:38 +0300

    Support UnitEnum

--- src/Concerns/HasNavigationGroupLabel.php
+++ src/Concerns/HasNavigationGroupLabel.php
@@ -6,14 +6,14 @@ use Closure;
 
 trait HasNavigationGroupLabel
 {
-    public function navigationGroupLabel(Closure | string $label): static
+    public function navigationGroupLabel(Closure | \UnitEnum | string $label): static
     {
         $this->navigationGroupLabel = $label;
 
         return $this;
     }
 
-    public function getNavigationGroupLabel(): Closure | string
+    public function getNavigationGroupLabel(): Closure | \UnitEnum | string
     {
         return $this->evaluate($this->navigationGroupLabel);
     }
